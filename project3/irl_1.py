dbf7fdefe82c407b895a2d4703f4ed5079cb361f
sherwin29 <sherwin.chsh@yahoo.com>
2018-05-20 03:41:00 -0700

    update irl process

--- project3/irl_1.py
+++ project3/irl_1.py
@@ -6,87 +6,78 @@ MaxState = 100
 MaxAction = 4
 Inf = 10000000
 
-def irl_process(state_num, optimal_action, trans_prob_matrix, penalty_lambda):  
+def irl_process(state_num, optimal_action, trans_prob_matrix, penalty_lambda, Rmax):  
     gamma = 0.8
     d = 300
 
     actions = {0, 1, 2, 3}
 
+    Ppart = np.zeros((d, state_num))
+    i = 0
+    for s in range(state_num):
+        for a in actions - {int(optimal_action[s])}:
+            Ppart[i, :] = np.dot(trans_prob_matrix[int(optimal_action[s]), s, :] - trans_prob_matrix[a, s, :], np.linalg.inv(np.eye(state_num) - gamma * trans_prob_matrix[int(optimal_action[s]), s, :]))
+            i += 1
+    Ppart = -1 * Ppart
+
+    Ppart_ones = np.ones((d, state_num))
+    i = 0
+    for s in range(state_num):
+        for a in actions - {int(optimal_action[s])}:
+            Ppart_ones[i, :] = np.eye(1, state_num, s)
+            i += 1
+
     # (1) objective
-    # order in expression: x1=R, x2=t, x3=u, x4=Rmax]
+    # order in expression: x1=R, x2=t, x3=u]
 
     # coefficient: C 
-    c1 = np.zeros(state_num)
-    c2 = np.ones(state_num)
-    c3 = -1 * penalty_lambda * np.ones(state_num)
-    c4 = np.zeros(state_num)
-    C = -1 * cvxopt.matrix(np.hstack([c1, c2, c3, c4]))
+    c1 = np.zeros((state_num,))
+    c2 = np.ones((state_num,))
+    c3 = -1 * penalty_lambda * np.ones((state_num,))
+    C = -1 * cvxopt.matrix(np.hstack([c1, c2, c3]))
 
 
     # (2) constraint: D
-    # constraint1: -1 * (Pa1 − Pa) * inv(I − gamma * Pa1) * R + T <= 0, for all i
-    D1_R = []
-    for state in range(state_num):
-        for action in actions - {optimal_action[state]}:
-            # (Pa1 − Pa) * inv(I − gamma * Pa1)
-            lp = np.dot(trans_prob_matrix[int(optimal_action[state]), state] - trans_prob_matrix[action, state], np.linalg.inv(np.eye(state_num) - gamma * trans_prob_matrix[int(optimal_action[state])]))
-            D1_R.append(lp)
-    D1_R = -1 * np.vstack(D1_R)
-    D1_t = []
-    for state in range(state_num):
-        for action in (actions - {optimal_action[state]}):
-            tmp = np.eye(1, state_num, state)
-            D1_t.append(tmp)
-    D1_t = np.vstack(D1_t)
-    D1_u = np.zeros((d, state_num))
-    D1_Rmax = np.zeros((d, state_num))
-    D1 = np.hstack([D1_R, D1_t, D1_u, D1_Rmax])
+    # constraint1:
+    D11 = Ppart
+    D12 = Ppart_ones
+    D13 = np.zeros((d, state_num))
+    D1 = np.hstack([D11, D12, D13])
 
-    # constraint2: -1 * (Pa1 − Pa) * inv(I − gamma * Pa1) * R <= 0
-    D2_R = []
-    for state in range(state_num):
-        for action in actions - {optimal_action[state]}:
-            # (Pa1 − Pa) * inv(I − gamma * Pa1)
-            lp = np.dot(trans_prob_matrix[int(optimal_action[state]), state] - trans_prob_matrix[action, state], np.linalg.inv(np.eye(state_num) - gamma * trans_prob_matrix[int(optimal_action[state])]))
-            D2_R.append(lp)
-    D2_R = -1 * np.vstack(D2_R)
-    D2_t = np.zeros((d, state_num))
-    D2_u = np.zeros((d, state_num))
-    D2_Rmax = np.zeros((d, state_num))
-    D2 = np.hstack([D2_R, D2_t, D2_u, D2_Rmax])
+    # constraint2:
+    D21 = Ppart
+    D22 = np.zeros((d, state_num))
+    D23 = np.zeros((d, state_num))
+    D2 = np.hstack([D21, D22, D23])
 
-    # constraint3: -R-u <= 0
-    D3_R = -1 * np.eye(state_num)
-    D3_t = np.zeros((state_num, state_num))
-    D3_u = -1 * np.eye(state_num)
-    D3_Rmax = np.zeros((state_num, state_num))
-    D3 = np.hstack([D3_R, D3_t, D3_u, D3_Rmax])
+    # constraint3:
+    D31 = -1 * np.eye(state_num)
+    D32 = np.zeros((state_num, state_num))
+    D33 = -1 * np.eye(state_num)
+    D3 = np.hstack([D31, D32, D33])
 
-    # constraint4: R-u <= 0
-    D4_R = np.eye(state_num)
-    D4_t = np.zeros((state_num, state_num))
-    D4_u = -1 * np.eye(state_num)
-    D4_Rmax = np.zeros((state_num, state_num))
-    D4 = np.hstack([D4_R, D4_t, D4_u, D4_Rmax])
+    # constraint4:
+    D41 = np.eye(state_num)
+    D42 = np.zeros((state_num, state_num))
+    D43 = -1 * np.eye(state_num)
+    D4 = np.hstack([D41, D42, D43])
 
-    # constraint5: -R-Rmax <=0
-    D5_R = -1 * np.eye(state_num)
-    D5_t = np.zeros((state_num, state_num))
-    D5_u = np.zeros((state_num, state_num))
-    D5_Rmax = -1 * np.eye(state_num)
-    D5 = np.hstack([D5_R, D5_t, D5_u, D5_Rmax])
+    # constraint5:
+    D51 = -1 * np.eye(state_num)
+    D52 = np.zeros((state_num, state_num))
+    D53 = np.zeros((state_num, state_num))
+    D5 = np.hstack([D51, D52, D53])
 
-    # constraint6: R-Rmax <=0
-    D6_R = np.eye(state_num)
-    D6_t = np.zeros((state_num, state_num))
-    D6_u = np.zeros((state_num, state_num))
-    D6_Rmax = -1 * np.eye(state_num)
-    D6 = np.hstack([D6_R, D6_t, D6_u, D6_Rmax])
+    # constraint6:
+    D61 = np.eye(state_num)
+    D62 = np.zeros((state_num, state_num))
+    D63 = np.zeros((state_num, state_num))
+    D6 = np.hstack([D61, D62, D63])
 
     D = cvxopt.matrix(np.vstack([D1, D2, D3, D4, D5, D6]))
 
     # B
-    B = cvxopt.matrix(np.zeros((1000,1)))
+    B = cvxopt.matrix(np.concatenate((np.zeros(8 * state_num,), np.full((2 * state_num, ), Rmax))).astype(np.double))
 
     results = cvxopt.solvers.lp(C, D, B)
     result = np.asarray(results["x"][:state_num], dtype=np.double).reshape((state_num,))
@@ -106,12 +97,12 @@ def format_trans(expert_optimal_action, trans_prob_matrix):
 
 
 
-def evaluate(state_num, expert_optimal_action, trans_prob_matrix, penalty_lambdas):
+def evaluate(state_num, expert_optimal_action, trans_prob_matrix, penalty_lambdas, Rmax):
     accuracy = []
     rewards_extracted = []
     expert_optimal_action, trans_prob_matrix = format_trans(expert_optimal_action, trans_prob_matrix)
     for penalty_lambda in penalty_lambdas:
-        reward_extracted = irl_process(state_num, expert_optimal_action, trans_prob_matrix, penalty_lambda)
+        reward_extracted = irl_process(state_num, expert_optimal_action, trans_prob_matrix, penalty_lambda, Rmax)
 
         # change format due to the function impolemenation
         reward_extracted = reward_extracted.reshape((10, 10)).tolist()
@@ -269,7 +260,7 @@ def generate_heapmap(R):
 # Question 11
 expert_optimal_action = PI
 penalty_lambdas = np.linspace(0, 5, 100)
-accuracy, rewards_extracted = evaluate(100, expert_optimal_action, trans_prob_matrix, penalty_lambdas)
+accuracy, rewards_extracted = evaluate(100, expert_optimal_action, trans_prob_matrix, penalty_lambdas, 1)
 plt.plot(penalty_lambdas, accuracy)
 plt.show()
 
@@ -294,7 +285,7 @@ arrow_map(policy)
 
 # Question 18
 penalty_lambdas2 = np.linspace(0, 5, 500)
-accuracy2, rewards_extracted2 = evaluate(100, expert_optimal_action2, trans_prob_matrix, penalty_lambdas)
+accuracy2, rewards_extracted2 = evaluate(100, expert_optimal_action2, trans_prob_matrix, penalty_lambdas, 100)
 plt.plot(penalty_lambdas2, accuracy2)
 plt.show()
 
