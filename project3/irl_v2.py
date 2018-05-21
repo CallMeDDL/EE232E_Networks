d38afc8a06db06223c0026ddf45ecf0a37fb3f18
CallMeDDL <liuty1317@gmail.com>
2018-05-21 14:58:25 -0700

    finish part 2 code

--- project3/irl_v2.py
+++ project3/irl_v2.py
@@ -1,7 +1,192 @@
 import numpy as np
+import copy
 import matplotlib.pyplot as plt
 import cvxopt
 
+def is_intended(act,s,t): # return whether next state is the intended state
+    if (act == 0 and s - 1 == t) or (act == 1 and s + 10 == t) or \
+    (act == 2 and s + 1 == t) or (act == 3 and s - 10 == t):
+        return True
+    else:
+        return False
+
+def is_interior(s):
+    x = s % 10
+    y = s // 10
+    if y == 0 or y == 9 or x == 0 or x == 9:
+        return False
+    else:
+        return True
+
+def is_edge(s):
+    x = s % 10
+    y = s // 10
+    if x == 0 and y in range(1, 9): # up edge 
+        return 0
+    elif y == 9 and x in range(1, 9): # right edge 
+        return 1
+    elif x == 9 and y in range(1, 9): # down edge
+        return 2
+    elif y == 0 and x in range(1, 9): # left edge
+        return 3
+    else:
+        return -1 # not an edge
+    
+def is_corner(s):
+    if s in [0,9,90,99]:
+        return True
+    else:
+        return False
+def get_prob_matrix(w): 
+    # up: 0, right: 1, down: 2, left: 3
+    Prob = np.zeros((4,100,100)) # s-state, t-state
+
+    
+    #corner_state = [(0,0),(9,9),(90,90),(99,99)] # stay in corner
+    corner_state = [0,9,90,99]
+    for act in range(0, 4):
+        for s in range(0, 100):
+            
+            if is_interior(s):     # s is inner state (has 4 directions)
+                for t in [s - 1, s + 1, s - 10, s + 10]: # all in range
+                    if is_intended(act,s,t): # correct state
+                        Prob[act,s,t] = 1 - w + w / 4
+                    else:
+                        Prob[act,s,t] = w / 4
+            elif is_edge(s) != -1:
+                if is_edge(s) == act: # off grid direction,stay
+                    Prob[act,s,s] = 1 - w + w / 4
+                else:
+                    Prob[act,s,s] = w / 4
+                if is_edge(s) == 0:
+                     for t in [s + 1, s - 10, s + 10]:
+                        if is_intended(act,s,t): # correct state
+                            Prob[act,s,t] = 1 - w + w / 4
+                        else:
+                            Prob[act,s,t] = w / 4
+                elif is_edge(s) == 2:  
+                     for t in [s - 1, s - 10, s + 10]:
+                        if is_intended(act,s,t): # correct state
+                            Prob[act,s,t] = 1 - w + w / 4
+                        else:
+                            Prob[act,s,t] = w / 4
+                else:
+                    for t in [s - 1, s + 1, s - 10, s + 10]:
+                        if t >= 0 and t <= 99:
+                            if is_intended(act,s,t): # correct state
+                                Prob[act,s,t] = 1 - w + w / 4
+                            else:
+                                Prob[act,s,t] = w / 4
+
+                        
+    #corner states:
+    # left-up corner
+    Prob[0, 0, 0] = 1 - w + w / 4 + w / 4
+    Prob[0, 0, 10] = w / 4
+    Prob[0, 0, 1] = w / 4
+    
+    Prob[1, 0, 0] = w / 4 + w / 4
+    Prob[1, 0, 10] = 1 - w + w / 4
+    Prob[1, 0, 1] = w / 4
+    
+    Prob[2, 0, 0] = w / 4 + w / 4
+    Prob[2, 0, 10] = w / 4
+    Prob[2, 0, 1] = 1 - w + w / 4
+    
+    Prob[3, 0, 0] = 1 - w + w / 4 + w / 4
+    Prob[3, 0, 10] = w / 4
+    Prob[3, 0, 1] = w / 4 
+    
+    # left-down corner
+    Prob[0, 9, 9] = w / 4 + w / 4
+    Prob[0, 9, 8] = 1 - w + w / 4
+    Prob[0, 9, 19] = w / 4
+    
+    Prob[1, 9, 9] = w / 4 + w / 4
+    Prob[1, 9, 8] = w / 4
+    Prob[1, 9, 19] = 1 - w + w / 4
+    
+    Prob[2, 9, 9] = 1 - w + w / 4 + w / 4
+    Prob[2, 9, 8] = w / 4
+    Prob[2, 9, 19] = w / 4
+    
+    Prob[3, 9, 9] = 1 - w + w / 4 + w / 4
+    Prob[3, 9, 8] = w / 4
+    Prob[3, 9, 19] = w / 4 
+    
+    # right-up corner
+    
+    Prob[0, 90, 90] = 1 - w + w / 4 + w / 4
+    Prob[0, 90, 91] = w / 4
+    Prob[0, 90, 80] = w / 4
+    
+    Prob[1, 90, 90] = 1 - w + w / 4 + w / 4
+    Prob[1, 90, 91] = w / 4
+    Prob[1, 90, 80] = w / 4
+    
+    Prob[2, 90, 90] = w / 4 + w / 4
+    Prob[2, 90, 91] = 1 - w + w / 4
+    Prob[2, 90, 80] = w / 4
+    
+    Prob[3, 90, 90] = w / 4 + w / 4
+    Prob[3, 90, 91] = w / 4
+    Prob[3, 90, 80] = 1 - w + w / 4
+    
+    # right-down corner
+    Prob[0, 99, 99] = w / 4 + w / 4
+    Prob[0, 99, 98] = 1 - w + w / 4
+    Prob[0, 99, 89] = w / 4
+    
+    Prob[1, 99, 99] = 1 - w + w / 4 + w / 4
+    Prob[1, 99, 98] = w / 4
+    Prob[1, 99, 89] = w / 4
+    
+    Prob[2, 99, 99] = 1 - w + w / 4 + w / 4
+    Prob[2, 99, 98] = w / 4
+    Prob[2, 99, 89] = w / 4
+    
+    Prob[3, 99, 99] = w / 4 + w / 4
+    Prob[3, 99, 98] = w / 4
+    Prob[3, 99, 89] = 1 - w + w / 4 
+
+    return Prob
+
+def get_opt_state_val(R,eps,w,gamma,Prob): # R: reward function, eps, w, gamma: discount factor
+    V = np.zeros((100,1))
+    
+    delta = float('inf') # infinity
+    #count = 0
+    corner = [0,9,90,99]
+    while delta > eps:
+        delta = 0
+        #print("iter:",count)
+        for s in range(0,100):
+            #v = V[s]
+            v = copy.deepcopy(V[s])
+            act_val = [] # length = 4
+            for act in range(0, 4): 
+                val = 0    
+                for t in range(0, 100):
+                    val += Prob[act, s, t] * (R[t % 10, t // 10] + gamma * V[t])
+                act_val.append(val)
+            V[s] = max(act_val)
+            delta = max(delta, abs(v - V[s])) # update delta
+            #PI[s] = act_val.index(V[s])
+        #count += 1
+    return V
+
+def get_opt_policy(V,R,Prob,gamma):
+    PI = np.zeros((100,1))
+    for s in range(0,100):
+        act_val = [] # length = 4
+        for act in range(0, 4): 
+            val = 0    
+            for t in range(0, 100):
+                val += Prob[act, s, t] * (R[t % 10, t // 10] + gamma * V[t])
+            act_val.append(val)
+        opt_val = max(act_val)
+        PI[s] = act_val.index(opt_val) 
+    return PI
 
 def irl_process(state_num, optimal_action, trans_prob_matrix, penalty_lambda, Rmax):  
     gamma = 0.8
@@ -93,7 +278,8 @@ def evaluate(state_num, expert_optimal_action, trans_prob_matrix, penalty_lambda
     accuracy = []
     rewards_extracted = []
     
-    expert_action = expert_optimal_action.reshape((10, 10)).T
+    #expert_action = expert_optimal_action.reshape((10, 10)).T
+    expert_action = expert_optimal_action
     
     for penalty_lambda in penalty_lambdas:
         reward_extracted = irl_process(state_num, expert_action, trans_prob_matrix, penalty_lambda, Rmax)
@@ -124,12 +310,24 @@ def generate_heapmap(R):
 
 # Question 11
 ##
+PI1 = np.array([[ 1,  1,  1,  1,  1,  1,  1,  2,  2,  2],
+    [ 2,  1,  1,  1,  1,  1,  2,  2,  2,  2],
+    [ 2,  2,  1,  1,  1,  2,  2,  2,  2,  2],
+    [ 2,  2,  2,  2,  2,  2,  2,  2,  2,  2],
+    [ 2,  2,  2,  1,  2,  2,  2,  2,  2,  2],
+    [ 2,  2,  1,  1,  1,  2,  2,  2,  2,  2],
+    [ 2,  1,  1,  1,  1,  1,  2,  2,  2,  2],
+    [ 1,  1,  1,  1,  1,  1,  1,  1,  2,  2],
+    [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  2],
+    [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+])
+
 expert_optimal_action = PI1
 Rmax = 1
 ##
 
-trans_prob_matrix = p
-penalty_lambdas = np.linspace(0, 5, 20) # change to 500
+trans_prob_matrix = get_prob_matrix(0.1)
+penalty_lambdas = np.linspace(0, 5, 100) # change to 500
 
 accuracy, rewards_extracted = evaluate(100, expert_optimal_action, trans_prob_matrix, penalty_lambdas, Rmax)
 plt.plot(penalty_lambdas, accuracy)
