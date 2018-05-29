bb97efaf53b4720ba528a716460b599c7ce5b96f
CallMeDDL <liuty1317@gmail.com>
2018-05-28 20:59:30 -0700

    Finish 2.1

--- project4/part2.R
+++ project4/part2.R
@@ -1,11 +1,10 @@
 library("igraph")
 
 # Q1: Create a weighted undirected movie network
+g = read.graph("par1_sampled_data_v1/movie_graph_edge_list.txt", format = "ncol", directed=FALSE)
 
-g = read.graph("movie_graph_edge_list.txt", format = "ncol", directed=TRUE)
-
-#png(filename="2_1.png")
-plot(degree.distribution(g, mode="in"))
+png(filename="2_1.png")
+plot(degree.distribution(g))
 
 
 
