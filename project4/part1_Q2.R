8f257b0d14c3f8a7bdce05e5701ec23305e0a430
CallMeDDL <liuty1317@gmail.com>
2018-05-28 22:30:55 -0700

    finish Q5

--- project4/part1_Q2.R
+++ project4/part1_Q2.R
@@ -13,11 +13,27 @@ g = read.graph("par1_sampled_data_v1/graph_edge_list.txt", format = "ncol", dire
 page_rank <- page_rank(g,damping=0.85, directed = TRUE)$vector
 top_actor <- head(sort(page_rank, decreasing=TRUE), 10)
 
-f_actor_idname_map = read.delim("par1_sampled_data_v1/actor_idname_map.txt",header = FALSE, sep="\t")
-f_movie_idname_map = read.delim("par1_sampled_data_v1/movie_idname_map.txt",header = FALSE, sep="\t")
+actor_idname_map = read.delim("par1_sampled_data_v1/actor_idname_map.txt",header = FALSE, sep="\t")
+actorid_movieids_map = read.delim("par1_sampled_data_v1/actorid_movieids_map.txt",header = FALSE, sep="\t")
+
+map_file <- file("par1_sampled_data_v1/actorid_movieids_map.txt", "r")
+actor_map <- list()
+i <- 1
+while ( TRUE ) {
+    line = readLines(map_file, n = 1)
+    if ( length(line) == 0 ) {break}
+    vector <- c()
+    for (substr in strsplit(line, "\t\t")){
+        vector <- c(vector, as.numeric(substr))
+    }
+    actor_map[[i]] <- vector
+    i = i + 1
+}
+close(con)
 
 # find name
-actor_name <- f_actor_idname_map$V3[as.numeric(names(top_actor))+1]
+actor_name <- actor_idname_map$V3[as.numeric(names(top_actor))+1]
+actor_name <- actor_idname_map$V3[as.numeric(names(top_actor))+1]
 print(actor_name)
 
 # degree and movies of those actors
@@ -25,7 +41,15 @@ deg <- degree(g, mode="in")
 for (actor_id in as.numeric(names(top_actor)))
 {
     print(deg[toString(actor_id)])
-    # id to id map 
+    print(length(actor_map[[as.numeric(actor_id) + 1]]) - 1)
 }
 
+# Q5
+actorid_list <- c(14503, 111298, 12812, 27258, 32389, 16878, 62774, 107832, 17285, 53248)
+names(actorid_list) <- c("Cruise, Tom", "Watson, Emma (II)", "Clooney, George", "Hanks, Tom", "Johnson, Dwayne (I)", "Depp, Johnny", "Smith, Will (I)", "Streep, Meryl", "DiCaprio, Leonardo", "Pitt, Brad") 
+for (actor_id in as.numeric(actorid_list))
+{
+    print(deg[toString(actor_id)])
+    print(length(actor_map[[as.numeric(actor_id) + 1]]) - 1)
+}
 
